e61bdfd348b95b70a37f89ff3808d33dd4803247
Young <iamduyang@hust.edu.cn>
2016-12-30 21:37:47 +0800

    a weak solution  using HashSet

--- 136weakVersion.java
+++ 136weakVersion.java
@@ -2,24 +2,22 @@ import java.util.HashSet;
 public class Solution {
     public int singleNumber(int[] nums) {
         HashSet<Integer> hh1 = new HashSet<Integer>();
-        HashSet<Integer> hh2 = new HashSet<Integer>();
+  
        
         for(int i=0;i<nums.length;i++){
-             hh1.add(nums[i]);  
-             hh2.add(nums[i]);  
+             if(hh1.contains(nums[i])){
+                hh1.remove(nums[i]);
+             }
+             else
+                 hh1.add(nums[i]);  
+            
              
         }
-        for(int i=0;i<nums.length;i++){
-            if(hh1.contains(nums[i])){
-            	hh1.remove(nums[i]);
-            }
-            else
-            	hh2.remove(nums[i]);
-       }
+      
         int returnNumAction =0;
-        for(int returnNum:hh2)
-        	returnNumAction =returnNum;
-        	
+        for(int returnNum:hh1)
+            returnNumAction =returnNum;
+            
         
         return returnNumAction;
     }
