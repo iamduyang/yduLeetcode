7777d9bbd2b555e9bf037914bc2171c301a5a239
Young <iamduyang@hust.edu.cn>
2016-12-31 11:39:45 +0800

    101 Symmetric Tree

---
+++ 101.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)
+        	return true;
+        else 
+        	return isSymmetricOutSide(root.right,root.left);
+    }
+    public boolean isSymmetricOutSide(TreeNode right,TreeNode left) {
+        if(right==null&&left==null)
+        	return true;
+        else  if(right!=null&&left==null)
+        	return false;
+        else  if(right==null&&left!=null)
+        	return false;
+        else if(right.val!=left.val)
+        	return false;
+        else	
+        	return isSymmetricOutSide(right.right,left.left)&&isSymmetricOutSide(right.left,left.right);
+    }
+
+}
