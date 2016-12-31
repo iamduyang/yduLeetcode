fd5587279c80759327dc28ebf4f80ddee03d8a53
Young <iamduyang@hust.edu.cn>
2016-12-31 23:45:35 +0800

    191. Number of 1 Bits

---
+++ 191.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+        	return 0
+        else:
+        	return n%2+self.hammingWeight(n/2) 
