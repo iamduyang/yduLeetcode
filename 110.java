ed641f218bbafddc32e5a47f1c5493411e414d02
Young <iamduyang@hust.edu.cn>
2016-12-31 14:18:30 +0800

    Problem 110, weak-- solutuon

--- 110.java
+++ 110.java
@@ -1,22 +1,22 @@
 public class Solution {
     public boolean isBalanced(TreeNode root) {
-    	if(root==null)
-        	return true;
-    	else if(!(isBalanced(root.left)&&isBalanced(root.right)))
-    		return false;
-    	else if(Math.abs(depthLeftRight(root.left)-depthLeftRight(root.right))>1)
-    		return false;
-    	else 
-    		return true;  
-    	
+        if(root==null)
+            return true;
+        else if(Math.abs(depthLeftRight(root.left)-depthLeftRight(root.right))>1)
+            return false;
+        else if(!isBalanced(root.left)||!isBalanced(root.right))
+            return false;
+        else 
+            return true;  
+        
     }
     public int depthLeftRight(TreeNode root) {
-    	if(root==null)
-    		return 0;
-    	int depthRight = depthLeftRight(root.left);
-    	int depthLeft = depthLeftRight(root.right);
-    	int lengthBig =(depthRight>depthLeft)?depthRight:depthLeft;
-    	return lengthBig+1;	
+        if(root==null)
+            return 0;
+        int depthRight = depthLeftRight(root.left);
+        int depthLeft = depthLeftRight(root.right);
+        int lengthBig =(depthRight>depthLeft)?depthRight:depthLeft;
+        return lengthBig+1; 
       
      }
     
