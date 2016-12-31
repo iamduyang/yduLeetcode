12089fc395ba2c14187b93ba9f620f3b60241d81
Young <iamduyang@hust.edu.cn>
2017-01-01 00:45:05 +0800

    190. Reverse Bits

--- 190.py
+++ 190.py
@@ -2,12 +2,12 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
-    	retrunNum =0
-    	count =n
+        retrunNum =0
+        
         for x in xrange(1,33):
-        	print x
-        	retrunNum = retrunNum+pow(2,32-x)*(count%2)
-        	count = count/2
+
+            retrunNum = retrunNum+pow(2,32-x)*(n%2)
+            n = n/2
         
         return retrunNum
         
