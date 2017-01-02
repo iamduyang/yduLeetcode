ad7033f4df72c7c18024ce5bc4ae9d24eead0470
Young <iamduyang@hust.edu.cn>
2017-01-03 00:30:01 +0800

    258-better.py run faster again

--- 258-better.py
+++ 258-better.py
@@ -1,13 +1,10 @@
 class Solution(object):
 	def addDigits(self, num):
- 
 		while num >9:
 			numOut = 0
 			while num>9:
 				numOut =numOut+ num%10
 				num =num/10
 			num = numOut+num
-			 
-
 		return num
 			
