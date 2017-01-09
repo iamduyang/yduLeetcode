f53b0b1fc7683c7d09244e804ed6c823eef79459
Young <iamduyang@hust.edu.cn>
2017-01-09 10:54:57 +0800

    24. Swap Nodes in Pairs

--- 24.java
+++ 24.java
@@ -1,33 +1,29 @@
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-    	ListNode returnListNode =head;
-    	if (head==null||head.next==null)
-    		return head;
-    	
-    	ListNode firstNode=head;
-    	ListNode secondNode=head.next;
-    	firstNode.next=secondNode.next;
-    	secondNode.next=firstNode;
-    	returnListNode=secondNode;
-    	ListNode tempheadNode=head;
+
+        if (head==null||head.next==null)
+            return head;
+         
+        ListNode firstNode=head;
+        ListNode secondNode=head.next;
+        firstNode.next=secondNode.next;
+        secondNode.next=firstNode;
+        ListNode returnListNode =secondNode;
         while(firstNode.next!=null){
-        	if(firstNode.next.next!=null){
-        		tempheadNode=firstNode.next;
-        		firstNode.next=firstNode.next.next;
-        	}
-        	else
-        		break;
-        	
-        		
-        		
-        	firstNode=tempheadNode;
-        	secondNode=firstNode.next;
-        	firstNode.next=secondNode.next;
-        	secondNode.next=firstNode;
-        	
-        	
+            if(firstNode.next.next!=null){
+                secondNode=firstNode.next;
+                firstNode.next=firstNode.next.next;
+            }
+            else
+                break;
+        
+            firstNode=secondNode;
+            secondNode=firstNode.next;
+            firstNode.next=secondNode.next;
+            secondNode.next=firstNode;
+    
         }
-        	
+            
         return returnListNode;
     }
 }
