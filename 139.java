a0a79d19b36e648703e9aba9d49caf64c95450b9
Young <iamduyang@hust.edu.cn>
2017-03-07 09:29:19 +0800

    139. Word Break

---
+++ 139.java
@@ -0,0 +1,19 @@
+/** 
+ * 139. Word Break
+ */
+public class Solution {
+	public boolean wordBreak(String s, List<String> wordDict) {
+        boolean [] breakable = new boolean[s.length()+1];
+        breakable[0] = true;
+
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(breakable[j]&&wordDict.contains(s.substring(j,i))){
+                    breakable[i] = true;
+                    break;
+                }
+            }
+        }
+        return breakable[s.length()];
+    }
+}
