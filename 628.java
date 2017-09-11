52dbfbeae96a718b9740b08f0d498320fd98c386
Young <iamduyang@hust.edu.cn>
2017-09-11 18:25:06 +0800

    628. Maximum Product of Three Numbers

---
+++ 628.java
@@ -0,0 +1,13 @@
+/** 
+ * 628. Maximum Product of Three Numbers
+ */
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        return Math.max(nums[len-1]*nums[len-2]*nums[len-3],nums[0]*nums[1]*nums[len-1]);
+       
+    }
+    
+    
+}
